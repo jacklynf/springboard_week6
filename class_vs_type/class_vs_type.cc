634e8b9b92afff348d009661339979279705eb6f
jackie_aws <jackie@aws_workspace>
2023-03-05 15:39:33 -0800

    class vs type complete

--- class_vs_type/class_vs_type.cc
+++ class_vs_type/class_vs_type.cc
@@ -16,9 +16,9 @@ int main ()
 
   cout << "=== append() test ===" << endl;
   cout << "x before: " << x << endl;
-  //x.append("_x");     // LINE 4
-  //rx.append("_rx");   // LINE 5
-  //crx.append("_crx"); // LINE 6
+  x.append("_x");     // LINE 4
+  rx.append("_rx");   // LINE 5
+  crx.append("_crx"); // LINE 6
 
   cout << "x after:  " << x << endl;
 
