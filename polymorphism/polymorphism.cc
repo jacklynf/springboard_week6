3d845add46264f98a6c86984ef004960f277bbb7
jackie_aws <jackie@aws_workspace>
2023-03-10 21:55:16 -0800

    polymorphism complete

--- polymorphism/polymorphism.cc
+++ polymorphism/polymorphism.cc
@@ -14,25 +14,43 @@ void poke (Animal const & animal)
 class Cow : public Animal
 {
   public:
-    virtual void make_sound () const override
+    void make_sound () const override
     { cout << "moo" << endl; }
 };
 
 class FullCow : public Cow
 {
   public:
-    virtual void make_sound () const override
+    void make_sound () const override
     { cout << "Ooof - I ate too much" << endl; }
+};
+
+class Pig : public Animal
+{
+  public:
+    void make_sound () const override
+    { cout << "Oink!" << endl; }
+};
+
+class ConfusedPig : public Pig
+{
+  public:
+    void make_sound () const override
+    { cout << "Moo?" << endl; }
 };
 
 int main ()
 {
   Cow c;
   FullCow fc;
+  Pig p;
+  ConfusedPig cp;
 
   poke(c);
   poke(fc);
-
+  poke(p);
+  poke(cp);
+  
   return 0;
 }
 
